6f74eea2632facd95015abbf62c75b3a3da94c7f
Caio Coimbra <164921594+CaioCoimbra@users.noreply.github.com>
2025-02-03 23:09:01 -0300

    Update bd.php

--- bd/bd.php
+++ bd/bd.php
@@ -1,8 +1,8 @@
 <?php 
     $servername = "";
-    $username = "u661331863_pdcadmin2";
-    $password = "CcS96184899"; 
-    $dbname = "u661331863_podcast2";
+    $username = "x";
+    $password = "y"; 
+    $dbname = "z";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     
